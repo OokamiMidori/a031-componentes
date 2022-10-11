4c4122923f1d4b57ed0f8753b7bc7754d68e460b
OokamiMidori <fabio.miranda.couto@gmail.com>
2022-10-11 13:35:45 -0300

    exercicio da tarde

---
+++ aula-componentes/aula-componentes/src/components/InfosUsuario.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function InfosUsuario (){
+    return (
+        <div>
+            <img src="https://static.miraheze.org/greatcharacterswiki/thumb/7/77/Pikachu.png/300px-Pikachu.png" alt=""/>
+            <p>Nome do Usuario: Pikachu</p>
+        </div>
+    )
+}
